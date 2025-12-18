e2dfdffb123aef2e7195a2f672f460d58f67827a
Jaakko Saaristo <jaakko.saaristo@gmail.com>
2025-12-18 05:30:07 +0000

    update

--- src/controllers/order.stream.controller.ts
+++ src/controllers/order.stream.controller.ts
@@ -6,10 +6,7 @@ import log from "#src/utils/logger";
 
 
 export async function orderStatusLongPollingHandler(req: express.Request, res: express.Response) {
-    res.setHeader('Content-Type', 'text/event-stream');
-    res.setHeader('Cache-Control', 'no-cache');
 
-    // Add the request to the map of active long polls
     activeLongPolls.set(req, res);
 
     req.on('close', () => {
